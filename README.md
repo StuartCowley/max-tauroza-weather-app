3f165c472c07c0096b800ff6d94009be37647457
Max Tauroza <maxtauroza@gmail.com>
2023-06-07 12:11:23 +0100

    Added title and background styling, AirBnb style g uide, linter and formatter

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Background Photo by Vadim Bogulov on Unsplash
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1 className="title">Weather App</h1>
     </div>
   );
 }
